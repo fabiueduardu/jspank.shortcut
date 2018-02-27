33ad18d50c6d03b6811cc3cafc1e1613e6451c47
Fabio Eduardo <fabiueduardu@hotmail.com>
2018-02-27 14:56:02 -0300

    add google translate shortcut, releases in zip, remove dlls

---
+++ src/jspank.crosscutting.googletranslate/Services/TranslateService.cs
@@ -0,0 +1,48 @@
+﻿using Google.Apis.Services;
+using jspank.core.services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using TranslationsResource = Google.Apis.Translate.v2.Data.TranslationsResource;
+
+namespace jspank.crosscutting.googletranslate.services
+{
+    public class TranslateService : ITranslateService
+    {
+        private const string TargetLanguage = "en";
+
+        public string Key
+        {
+            get
+            {
+                var value = ConfigurationManager.AppSettings["GoogleApiKeyFilePath"] as string;
+
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("GoogleApiKeyFilePath key not found on app.config");
+
+                return value;
+            }
+        }
+
+        public ICollection<string> Translate(string value, string targetLanguage = TargetLanguage)
+        {
+            var service = new Google.Apis.Translate.v2.TranslateService(new BaseClientService.Initializer()
+            {
+                ApiKey = this.Key,
+                ApplicationName = this.GetType().FullName
+            });
+
+            if (string.IsNullOrEmpty(targetLanguage))
+                targetLanguage = TargetLanguage;
+
+            var response = service.Translations.List(new[] { value }, targetLanguage).Execute();
+            var result = new Collection<string>();
+
+            foreach (TranslationsResource translation in response.Translations)
+                result.Add(translation.TranslatedText);
+
+            return result;
+        }
+    }
+}
